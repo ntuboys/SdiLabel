df00d799d680b987a654d20aeb2a22ad2e35a2c0
Arek <49416568+arekscode@users.noreply.github.com>
2020-04-25 14:23:52 +0100

    commented tests

--- tests.cpp
+++ tests.cpp
@@ -3,50 +3,54 @@
 #include <vector>
 
 namespace Tests {
+// testing linked list fuctionality
 bool linkedList() {
   LinkedList list;
-  list.add("first");
+  list.add("first");  // add elements
   list.add("second");
-  if (!(list.pop() == "second") || !(list.pop() == "first")) {
+  if (!(list.pop() == "second") || !(list.pop() == "first")) {  // check if they were actually aded
     return false;
   }
   return true;
 }
+// testing the search algorithm
 bool searchAlgorithm() {
   LinkedList list;
-  list.add("first");
+  list.add("first");  // add elements
   list.add("second");
-  if (!(list.find("first") == 1)) {
+  if (!(list.find("first") == 1)) { // see if the returned value is what was expected
     return false;
   }
   return true;
 }
+// sorting algorithm test
 bool sortAlgorithm() {
   LinkedList list;
-  list.add("first");
+  list.add("first");  // add elements
   list.add("second");
   list.sort();
-  if (!(list.find("first") == 0)) {
+  if (!(list.find("first") == 0)) { // see if the elements are in expected order
     return false;
   }
   return true;
 }
+// integration test
 bool intTest() {
   bool passed = true;
   LinkedList list;
 
-  list.add("first");
+  list.add("first");  // add elements
   list.add("second");
-  if (!(list.pop() == "second") || !(list.pop() == "first")) {
+  if (!(list.pop() == "second") || !(list.pop() == "first")) { // check if they were actually aded
     passed = false;
   }
   list.add("first");
   list.add("second");
-  if (!(list.find("first") == 1)) {
+  if (!(list.find("first") == 1)) { // see if the returned value is what was expected
     passed = false;
   }
   list.sort();
-  if (!(list.find("first") == 0)) {
+  if (!(list.find("first") == 0)) { // see if the elements are in expected order
     passed = false;
   }
 
@@ -55,6 +59,7 @@ bool intTest() {
 } // namespace Tests
 
 int main() {
+  // if any of the tests failed return non-zero
   bool failed = false;
   if (!Tests::linkedList()) {
     std::cout << "Linked list test failed" << std::endl;
